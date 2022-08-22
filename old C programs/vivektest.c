d9c2e24dd83a0275d6cacf9267e36e2953c60436
Rajan Chaurasia <chaurasiarajan3013@gmail.com>
2022-08-22 13:10:40 +0530

    test

--- old C programs/vivektest.c
+++ old C programs/vivektest.c
@@ -9,9 +9,9 @@ int main()
     {
       
        scanf("%d%d",&a,&b);
-       if(a>b)
+       if(a<b)
          Ans[i]=0;
-       else if(a<b)
+       else if(a>b)
          Ans[i]=1;
         else 
          Ans[i]=2;
@@ -19,4 +19,25 @@ int main()
     }
     for(i=0;i<t;i++)
       printf("%s\n",str[Ans[i]]);
+}
+
+#include <stdio.h>
+int main()
+{   int t,i=0, a,b;
+    scanf("%d",&t);
+    int Ans[t];
+   
+    while(i<t)
+    {
+      
+       scanf("%d%d",&a,&b);
+       if(a<b)
+         printf("FIRST\n");
+       else if(a>b)
+          printf("SECOND\n");
+        else 
+          printf("ANY\n");
+       i++;
+    }
+   
 }
